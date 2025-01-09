febfd93f5640d5270ca3d7bd7478a8080bfdb89c
Dhvan Shah <dhvanshah15@gmail.com>
2025-01-08 19:23:43 -0500

    mid project commit

--- kinematics.py
+++ kinematics.py
@@ -2,16 +2,14 @@ import sympy as sp
 
 base_radius, proximal_length, distal_length, end_effector_radius, offset_distance = 0.165, 0.2, 0.4, 0.2, 0.0562
 
-def calculate_end_effector_position(q):
-    q11, q12, q13 = q[0], q[1], q[2]
+def calculate_end_effector_position(arm_angles):
     return sp.Matrix([
-        distal_length * sp.sin(q12) * sp.sin(q13),
-        base_radius - offset_distance + proximal_length * sp.cos(q11) + distal_length * sp.cos(q12),
-        proximal_length * sp.sin(q11) + distal_length * sp.sin(q12) * sp.cos(q13)
+        distal_length * sp.sin(arm_angles[1]) * sp.sin(arm_angles[2]),
+        base_radius - offset_distance + proximal_length * sp.cos(arm_angles[0]) + distal_length * sp.cos(arm_angles[1]),
+        proximal_length * sp.sin(arm_angles[0]) + distal_length * sp.sin(arm_angles[1]) * sp.cos(arm_angles[2])
     ])
 
 def calculate_constraint(joint_angles):
-    joint_angles_1, joint_angles_2, joint_angles_3 = [joint_angles[1], joint_angles[2], joint_angles[3]]
     R_2 = sp.Matrix([
         [sp.cos(sp.rad(120)), -sp.sin(sp.rad(120)), 0],
         [sp.sin(sp.rad(120)), sp.cos(sp.rad(120)), 0],
@@ -25,8 +23,8 @@ def calculate_constraint(joint_angles):
     ])
 
     return sp.Matrix([
-        psi(q1) - R_2 * psi(q2),
-        psi(q1) - R_3 * psi(q3)
+        calculate_end_effector_position(joint_angles[0]) - R_2 * calculate_end_effector_position(joint_angles[1]),
+        calculate_end_effector_position(joint_angles[0]) - R_3 * calculate_end_effector_position(joint_angles[2])
     ])
 
 def calculate_constraint(distal_angles, proximal_angles):
@@ -42,4 +40,60 @@ def calculate_forward_kinematics(proximal_angles, initial_guess, tolerance=1e-5,
     distal_angles_guess = sp.Matrix(initial_guess)
 
     for _ in range(max_iterations):
-        constraint_value = 
+        constraint_values = calculate_constraint(distal_angles_guess, proximal_angles)
+        constraint_jacobian = constraint_values.jacobian(distal_angles_guess)
+
+        if(all(sp.Abs(constraint_value) < tolerance for constraint_value in constraint_values)):
+            break
+
+        delta_angle = -constraint_jacobian.inv() * constraint_values
+        distal_angles_guess += delta_angle
+
+    return distal_angles_guess
+
+def calculate_inverse_kinematics_single(end_effector_position):
+    distal_twist_angle_s = sp.symbols("distal_twist_angle_s")
+    eq = sp.Eq(sp.sqrt(proximal_length**2 - (end_effector_position[2] - end_effector_position[0] * sp.cot(distal_twist_angle_s))**2) \
+         - sp.sqrt(distal_length**2 - end_effector_position[0]**2 * sp.csc(distal_twist_angle_s)**2) \
+         + base_radius - end_effector_radius - end_effector_position[1], 0)
+    
+    initial_guess = sp.asin(end_effector_position[0] / distal_length)
+
+    print(type(eq))
+    distal_twist_angle = sp.nsolve(eq, distal_twist_angle_s, initial_guess)
+
+    distal_angle = sp.pi - sp.asin(end_effector_position[0] / (distal_length * sp.sin(distal_twist_angle)))
+    proximal_angle = sp.asin((end_effector_position[2] - distal_length * sp.sin(distal_angle) * sp.cos(distal_twist_angle)) / proximal_length)
+
+    return [proximal_angle, distal_angle, distal_twist_angle]
+
+def calculate_inverse_kinematics(end_effector_position):
+    arm_1 = calculate_inverse_kinematics_single(end_effector_position)
+
+    R_2 = sp.Matrix([
+        [sp.cos(sp.rad(120)), -sp.sin(sp.rad(120)), 0],
+        [sp.sin(sp.rad(120)), sp.cos(sp.rad(120)), 0],
+        [0, 0, 1]
+    ])
+
+    rotated_position_2 = R_2 * sp.Matrix(end_effector_position)
+    arm_2 = calculate_inverse_kinematics_single(rotated_position_2)
+
+    R_3 = sp.Matrix([
+        [sp.cos(sp.rad(240)), -sp.sin(sp.rad(240)), 0],
+        [sp.sin(sp.rad(240)), sp.cos(sp.rad(240)), 0],
+        [0, 0, 1]
+    ])
+
+    rotated_position_3 = R_3 * sp.Matrix(end_effector_position)
+    arm_3 = calculate_inverse_kinematics_single(rotated_position_3)
+
+    proximal_angles = [arm_1[0], arm_2[0], arm_3[0]]
+    distal_angles = [arm_1[1], arm_1[2], arm_2[1], arm_2[2], arm_3[1], arm_3[2]]
+
+    return proximal_angles, distal_angles
+
+
+
+sample_end_effector_pos = [0.0, 1.0, 0.0]
+print(calculate_inverse_kinematics(sample_end_effector_pos))
---
+++ test.py
@@ -0,0 +1,5 @@
+import sympy as sp
+
+print(sp.rad(120))
+
+print(sp.pi * 120 / 180)
