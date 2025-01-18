a8181db0916cc24f43cedfd45a8e065c2f7b2f64
Dhvan Shah <dhvanshah15@gmail.com>
2025-01-18 13:50:44 -0500

    optimized jacobian creation by caching symbolized jacobains

--- jacobian.py
+++ jacobian.py
@@ -1,7 +1,9 @@
 import sympy as sp
+import numpy as np
+import dill
 from utils import calculate_end_effector_position, calculate_constraint
 
-def calculate_control_jacobian(distal_angles_numeric, proximal_angles_numeric):
+def save_control_jacobian(filename="control_jacobian.pkl"):
     distal_angles = sp.Matrix(sp.symbols('d1:7'))
     proximal_angles = sp.Matrix(sp.symbols('p1:4'))
 
@@ -16,28 +18,44 @@ def calculate_control_jacobian(distal_angles_numeric, proximal_angles_numeric):
     solved_term = jac_constraint_distal.LUsolve(jac_constraint_proximal)
     control_jacobian = jac_end_distal @ -solved_term + jac_end_proximal
 
-    control_jacobian_func = sp.lambdify([distal_angles, proximal_angles], control_jacobian, modules="numpy")
+    control_jacobian_func = sp.lambdify([sp.symbols('d1:7'), sp.symbols('p1:4')], control_jacobian, modules="numpy")
+
+    with open(filename, "wb") as f:
+        dill.dump(control_jacobian_func, f)
+
+def calculate_control_jacobian(distal_angles_numeric, proximal_angles_numeric, filename="control_jacobian.pkl"):
+    with open(filename, "rb") as f:
+        control_jacobian_func = dill.load(f)
     return control_jacobian_func(distal_angles_numeric, proximal_angles_numeric)
 
-def calculate_gravity_compensation(distal_angles_numeric, proximal_angles_numeric):
-    # Define symbolic variables
+def save_gravity_compensation(filename="gravity_compensation.pkl"):
     distal_angles = sp.Matrix(sp.symbols('d1:7'))
     proximal_angles = sp.Matrix(sp.symbols('p1:4'))
     gravity = sp.Matrix([0, 0, 9.81])
 
-    # Compute end-effector position and gravity torque
     end_effector_pos = calculate_end_effector_position(distal_angles, proximal_angles)
     jac_end_distal = end_effector_pos.jacobian(distal_angles)
 
     gravity_torque = jac_end_distal.T @ gravity
 
-    # Convert to numerical function and evaluate
     gravity_torque_func = sp.lambdify([distal_angles, proximal_angles], gravity_torque, modules="numpy")
+
+    with open(filename, "wb") as f:
+        dill.dump(gravity_torque_func, f)
+
+def calculate_gravity_compensation(distal_angles_numeric, proximal_angles_numeric, filename="gravity_compensation.pkl"):
+    with open(filename, "rb") as f:
+        gravity_torque_func = dill.load(f)
     return gravity_torque_func(distal_angles_numeric, proximal_angles_numeric)
 
 
 distal_angles_numeric = [1.0485, 1e-5, 1.0485, 1e-5, 1.0485, 1e-5]
 proximal_angles_numeric = [0.4556, 0.4556, 0.4556]
 
-# print(calculate_control_jacobian(distal_angles_numeric, proximal_angles_numeric))
+SAVE=False
+if(SAVE):
+    save_control_jacobian()
+    save_gravity_compensation()
+
+print(calculate_control_jacobian(distal_angles_numeric, proximal_angles_numeric))
 print(calculate_gravity_compensation(distal_angles_numeric, proximal_angles_numeric))
